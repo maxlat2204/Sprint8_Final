0ee0ec28fb543a09f5072698ff1c128f8ebcd6a4
maxlat2204 <maxlat2013@yandex.ru>
2025-05-07 00:00:53 +0500

    Update Praktikum.java

--- src/main/java/praktikum/Praktikum.java
+++ src/main/java/praktikum/Praktikum.java
@@ -1,20 +1,20 @@
-// package praktikum;
+package praktikum;
 
-// import org.junit.Test;
+import org.junit.Test;
 
-// public class Praktikum {
-//     public static void main(String[] args) {
-//             String name = "Тимоти Шаламе";
-//             var account = new Account(name);
-//             account.checkNameToEmboss(name);
+public class Praktikum {
+    public static void main(String[] args) {
+            String name = "Тимоти Шаламе";
+            var account = new Account(name);
+            account.checkNameToEmboss(name);
 
 
 
-//         /*
-//              В этом методе заложи логику работы с классом Account.
-//              Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
-//              и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
-//          */
-//     }
+        /*
+             В этом методе заложи логику работы с классом Account.
+             Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
+             и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
+         */
+    }
 
-// }
+}
